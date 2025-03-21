8f2a28e82e1f2f9c1712b2d33eb59cde02c4cbcb
mateoherreno <mateoherre@outlook.es>
2025-03-20 23:53:45 -0500

    añadadidos CORS a las rutas

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-const path = require('path');
 const express = require('express');
 const app = express();
+const cors = require("cors");
 const router = require('./backend/routes/routes');
 const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 
+app.use(cors({
+  origin: "*", // Permite todas las solicitudes (puedes cambiarlo a tu frontend específico)
+  methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 
-app.use(express.urlencoded({extended:false}))
-app.use(express.json())
-app.use('/', router())
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'/frontend/views/pages'));
-app.use(express.static('./frontend/views/static'))
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use('/', router());
+app.use(cors());
 
 
 app.listen(PORT, () => {
     console.log('Servidor corriendo en', PORT);
 });
+
+
+
